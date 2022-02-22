3b222a084ac17ca352aa07186c0c8180ec0b6217
Alex Cooksey <aecooksey2651@gmail.com>
2022-02-22 15:00:27 -0500

    Create models and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,20 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding data..."
 
 # Seed your database here
+200.times do
+    Student.create(
+        name: Faker::Student.name,
+        class_year: Faker::Student.class_year,
+    )
+end
+
+40.times do
+    course = Course.create(
+        title: Faker::Course.title,
+        description: Faker::Course.description,
+        department_id: Faker::Course.department_id
+    )
+
+    
 
 puts "✅ Done seeding!"

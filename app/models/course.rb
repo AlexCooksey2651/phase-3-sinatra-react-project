eadef649fb5e48c1a0d2398fd7772856b3e64f82
Alex Cooksey <92754374+AlexCooksey2651@users.noreply.github.com>
2022-06-08 15:34:47 -0400

    Remove test method from Course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,9 +1,5 @@
 class Course < ActiveRecord::Base
     has_many :student_courses
     has_many :students, through: :student_courses
-    belongs_to :department
-
-    def kittyKat
-        binding.irb
-    end    
+    belongs_to :department  
 end
